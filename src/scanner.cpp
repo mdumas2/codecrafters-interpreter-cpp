36b597fcc1b8a7f21efeba4aa6c88ec57c93222e
Mateus <52970228+mdumas2@users.noreply.github.com>
2025-04-10 00:46:45 -0300

    -

--- src/scanner.cpp
+++ src/scanner.cpp
@@ -5,6 +5,77 @@
 #include <algorithm>
 #include <iomanip>
 
+enum class TokenType {
+    LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
+    COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
+
+    BANG, BANG_EQUAL,
+    EQUAL, EQUAL_EQUAL,
+    GREATER, GREATER_EQUAL,
+    LESS, LESS_EQUAL,
+
+    IDENTIFIER, STRING, NUMBER,
+
+    // Keywords
+    AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR,
+    PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE,
+
+    ERROR, EOF_TOKEN
+};
+
+std::string tokenTypeToString(TokenType type) {
+    switch (type) {
+        case TokenType::LEFT_PAREN:  return "LEFT_PAREN";
+        case TokenType::RIGHT_PAREN: return "RIGHT_PAREN";
+        case TokenType::LEFT_BRACE:  return "LEFT_BRACE";
+        case TokenType::RIGHT_BRACE: return "RIGHT_BRACE";
+        case TokenType::COMMA:       return "COMMA";
+        case TokenType::DOT:         return "DOT";
+        case TokenType::MINUS:       return "MINUS";
+        case TokenType::PLUS:        return "PLUS";
+        case TokenType::SEMICOLON:   return "SEMICOLON";
+        case TokenType::SLASH:       return "SLASH";
+        case TokenType::STAR:        return "STAR";
+        case TokenType::BANG:        return "BANG";
+        case TokenType::BANG_EQUAL:  return "BANG_EQUAL";
+        case TokenType::EQUAL:       return "EQUAL";
+        case TokenType::EQUAL_EQUAL: return "EQUAL_EQUAL";
+        case TokenType::GREATER:     return "GREATER";
+        case TokenType::GREATER_EQUAL: return "GREATER_EQUAL";
+        case TokenType::LESS:        return "LESS";
+        case TokenType::LESS_EQUAL:  return "LESS_EQUAL";
+        case TokenType::IDENTIFIER:  return "IDENTIFIER";
+        case TokenType::STRING:      return "STRING";
+        case TokenType::NUMBER:      return "NUMBER";
+        case TokenType::AND:         return "AND";
+        case TokenType::CLASS:       return "CLASS";
+        case TokenType::ELSE:        return "ELSE";
+        case TokenType::FALSE:       return "FALSE";
+        case TokenType::FOR:         return "FOR";
+        case TokenType::FUN:         return "FUN";
+        case TokenType::IF:          return "IF";
+        case TokenType::NIL:         return "NIL";
+        case TokenType::OR:          return "OR";
+        case TokenType::PRINT:       return "PRINT";
+        case TokenType::RETURN:      return "RETURN";
+        case TokenType::SUPER:       return "SUPER";
+        case TokenType::THIS:        return "THIS";
+        case TokenType::TRUE:        return "TRUE";
+        case TokenType::VAR:         return "VAR";
+        case TokenType::WHILE:       return "WHILE";
+        case TokenType::ERROR:       return "ERROR";
+        case TokenType::EOF_TOKEN:         return "EOF";
+        default:                return "UNKNOWN";
+    }
+}
+
+struct Token {
+    TokenType type;
+    std::string lexeme;
+    std::string literal;
+    int line;
+};
+
 std::unordered_set<std::string> reservedWords = {
     "if", "else", "while", "for", "return", "true", "false", "class", "var", "fun", "print", "nil", "this", "super"
 };
