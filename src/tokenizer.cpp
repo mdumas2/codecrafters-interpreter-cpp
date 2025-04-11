3d46d37dc483f140b819db9ad4d210e1342267db
Mateus <52970228+mdumas2@users.noreply.github.com>
2025-04-11 01:07:08 -0300

    -

--- src/tokenizer.cpp
+++ src/tokenizer.cpp
@@ -63,15 +63,15 @@ std::pair<std::vector<Token>, int> Tokenizer::scan_tokens(std::string& src) {
 }
 
 std::string Tokenizer::get_name(TokenType type) {
-    auto it = TOKEN_INFO_MAP.find(type);
-    if (it != TOKEN_INFO_MAP.end()) {
+    auto it = get_map().find(type);
+    if (it != get_map().end()) {
         return it->second.name;
     }
     return "UNKNOWN";
 }
 
 TokenType Tokenizer::get_token_type_for_identifier(const std::string& word) {
-    for (const auto& [type, info] : TOKEN_INFO_MAP) {
+    for (const auto& [type, info] : get_map()) {
         if (info.is_keyword && info.lexeme == word) {
             return type;
         }
