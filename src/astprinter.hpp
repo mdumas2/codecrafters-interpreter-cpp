28df86c219c98c5b529a711eabc32180dbcd2b8d
Mateus <52970228+mdumas2@users.noreply.github.com>
2025-04-14 22:16:31 -0300

    -

--- src/astprinter.hpp
+++ src/astprinter.hpp
@@ -1,36 +1,51 @@
-#pragma once
+#ifndef AST_PRINTER_HPP
+#define AST_PRINTER_HPP
 
 #include "expr.hpp"
 
-#include <string>
-#include <vector>
-#include <sstream>
+namespace lox {
 
-class AstPrinter : public ExprVisitor<std::string> {
-private:
-    template<typename... ExprArgs>
-    std::string parenthesize(const std::string& name, const ExprArgs&... exprs);
-
-    std::string parenthesizeList(const std::string& name, const std::vector<const Expr*>& exprs);
-
-    std::string anyToString(const std::any& value) const;
-
-
+class AstPrinter : public ExprVisitor {
 public:
-    std::string print(const Expr& expr);
+    std::string print(const Expr& expr) {
+        return expr.accept(*this);
+    }
 
-    std::string visitBinaryExpr(const Binary& expr) override;
-    std::string visitGroupingExpr(const Grouping& expr) override;
-    std::string visitLiteralExpr(const Literal& expr) override;
-    std::string visitUnaryExpr(const Unary& expr) override;
+    std::string visitBinaryExpr(const Binary& expr) override {
+        return parenthesize(expr.operator_.lexeme, {expr.left.get(), expr.right.get()});
+    }
+
+    std::string visitGroupingExpr(const Grouping& expr) override {
+        return parenthesize("group", {expr.expression.get()});
+    }
+
+    std::string visitLiteralExpr(const Literal& expr) override {
+        if (std::holds_alternative<std::monostate>(expr.value)) return "nil";
+        if (std::holds_alternative<double>(expr.value))
+            return std::to_string(std::get<double>(expr.value));
+        if (std::holds_alternative<std::string>(expr.value))
+            return std::get<std::string>(expr.value);
+        if (std::holds_alternative<bool>(expr.value))
+            return std::get<bool>(expr.value) ? "true" : "false";
+        return "";
+    }
+
+    std::string visitUnaryExpr(const Unary& expr) override {
+        return parenthesize(expr.operator_.lexeme, {expr.right.get()});
+    }
+
+private:
+    std::string parenthesize(const std::string& name, const std::vector<const Expr*>& exprs) {
+        std::string result = "(" + name;
+        for (const auto* expr : exprs) {
+            result += " ";
+            result += expr->accept(*this);
+        }
+        result += ")";
+        return result;
+    }
 };
 
+} // namespace lox
 
-template<typename... ExprArgs>
-std::string AstPrinter::parenthesize(const std::string& name, const ExprArgs&... exprs) {
-    std::ostringstream builder;
-    builder << "(" << name;
-    ((builder << " " << exprs.accept(*this)), ...);
-    builder << ")";
-    return builder.str();
-}
+#endif // AST_PRINTER_HPP
